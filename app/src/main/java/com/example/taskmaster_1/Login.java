31d6184dff2f1ace02baba1e2005a04ee1a82e7a
abdallahAlabed <git config --global user.email>
2021-09-14 03:22:07 +0300

    lab36

---
+++ app/src/main/java/com/example/taskmaster_1/Login.java
@@ -0,0 +1,63 @@
+package com.example.taskmaster_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Todo;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button logInBtn = findViewById(R.id.loginButt);
+
+        EditText userName = findViewById(R.id.loginUserNameTxt);
+        EditText password = findViewById(R.id.loginPasswordTxt);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        Amplify.Auth.signIn(
+                "abdallahAlabed1",
+                "Abdllah321",
+                result -> Log.i("AuthQuickstart", result.isSignInComplete() ? "Sign in succeeded" : "Sign in not complete"),
+                error -> Log.e("AuthQuickstart", error.toString())
+        );
+
+                Toast.makeText(getApplicationContext(), "hallelooya!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button startBtn = findViewById(R.id.start);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getStarted = new Intent(Login.this, MainActivity.class);
+                startActivity(getStarted);
+            }
+        });
+        Button goToSinUpBtn = findViewById(R.id.goToSinUp);
+        goToSinUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sinUp = new Intent(Login.this, SinUp.class);
+                startActivity(sinUp);
+            }
+        });
+
+    }
+}
